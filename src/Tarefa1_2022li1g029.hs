ebf5f9e2a94f2be1a7923e6b5712f7b3f89d7a87
Gonçalo Freitas <a104350@alunos.uminho.pt>
2022-11-06 12:05:53 +0000

    Tarefa 1, 1 ponto completo, 2 ponto só dá se for falso

--- src/Tarefa1_2022li1g029.hs
+++ src/Tarefa1_2022li1g029.hs
@@ -10,13 +10,29 @@ module Tarefa1_2022li1g029 where
 
 import LI12223
 
+--1--
 mapaValido :: Mapa -> Bool
-mapaValido (Mapa lr ((tr,lo):t)) 
-                        |tr == Relva = not (elem Carro lo || elem Tronco lo)  
-                        |otherwise = True 
+mapaValido (Mapa lr l) = mapaValido1 (Mapa lr l)
 
-mapaValido (Mapa lr ((Rio v),lo):t)
-                        |(elem Carro lo || elem Tronco lo) = False
-                        |otherwise = True 
+mapaValido1 :: Mapa -> Bool
+mapaValido1 (Mapa lr []) = True
+mapaValido1 (Mapa lr ((Relva,lo):t)) 
+        |(elem Carro lo || elem Tronco lo) = False  
+        |otherwise = mapaValido1 (Mapa lr t)
+ 
+mapaValido1 (Mapa lr (((Rio vr),lo):t))
+        |(elem Carro lo || elem Arvore lo) = False 
+        |otherwise = mapaValido1 (Mapa lr t)
+        
+mapaValido1 (Mapa lr (((Estrada ve),lo):t))
+        |(elem Arvore lo || elem Tronco lo) = False
+        |otherwise = mapaValido1 (Mapa lr t)
+
+--2--
+mapaValido2 :: Mapa -> Bool
+mapaValido2 (Mapa lr []) = True  
+mapaValido2 (Mapa lr (((Rio vr),lo): ((Rio vr1),lo1) :t))
+                |(vr > 0 && vr1 > 0) || (vr < 0 && vr1 < 0) || (vr == 0 && vr1 == 0) = False
+                |otherwise = mapaValido2 (Mapa lr ((Rio vr1),lo1):t)
 
 
