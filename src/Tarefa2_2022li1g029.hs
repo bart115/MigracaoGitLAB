3919917d46c4454be18d8feca2dcd4d6fe12cd7c
a104010 <a104010@alunos.uminho.pt>
2022-11-08 14:41:12 +0000

    Tarefa 2 corroborada

--- src/Tarefa2_2022li1g029.hs
+++ src/Tarefa2_2022li1g029.hs
@@ -16,14 +16,11 @@ import LI12223
 --função auxiliar que dá os terrenos válidos para a proxima linha
 
 proximosTerrenosValidos :: Mapa -> [Terreno]
-proximosTerrenosValidos (Mapa _ []) = [Rio 0, Estrada 0, Relva]                                                              --quando não tem nenhum terreno
-proximosTerrenosValidos (Mapa _ [(Rio _, _),(Rio _, _),(Rio _, _),(Rio _, _),_]) = [Estrada 0, Relva]                        --quando tem 4 rios seguidos 
-proximosTerrenosValidos (Mapa _ [(Rio _,_),_]) = [Rio 0,Estrada 0,Relva]                                                     --quando tem um rio 
-proximosTerrenosValidos (Mapa _ [(Estrada _,_),(Estrada _ ,_),(Estrada _,_),(Estrada _,_),(Estrada _, _),_ ])=[Rio 0, Relva] --quando tem 5 estradas seguidas
-proximosTerrenosValidos (Mapa _ [(Estrada _,_),_]) = [Rio 0,Estrada 0,Relva]                                                 --quando tem uma estrada
-proximosTerrenosValidos (Mapa _ [(Relva ,_),(Relva ,_),(Relva ,_),(Relva ,_),(Relva ,_)])=[Estrada 0 , Rio 0]                --quando tem 5 relvas seguidas
-proximosTerrenosValidos (Mapa _ [(Relva,_),_]) = [Rio 0,Estrada 0,Relva]                                                     --quando tem uma relva
-
+proximosTerrenosValidos (Mapa l []) = [Rio 0, Estrada 0, Relva]                                                                       --quando não tem nenhum terreno
+proximosTerrenosValidos (Mapa l (t:[(Rio v1, ob1),(Rio v2, ob2),(Rio v3, ob3),(Rio v4, ob4)])) = [Estrada 0, Relva]                   --exceção: quando tem 4 rios seguidos 
+proximosTerrenosValidos (Mapa l (t:[(Estrada v1,ob1),(Estrada _ ,_),(Estrada _,_),(Estrada _,_),(Estrada _, _)]) )=[Rio 0, Relva]     --exceção: quando tem 5 estradas seguidas
+proximosTerrenosValidos (Mapa l (t:[(Relva ,_),(Relva ,_),(Relva ,_),(Relva ,_),(Relva ,_)]))=[Estrada 0 , Rio 0]                     --exceção: quando tem 5 relvas seguidas                                           
+proximosTerrenosValidos (Mapa l t) = [Rio 0, Estrada 0, Relva]                                                                        --restantes casos
 
 
 --função auxiliar que dá o terreno aleatório da proxima linha 
@@ -32,15 +29,13 @@ terrenoaleatorio::Mapa->Int->Terreno
 terrenoaleatorio (Mapa x l) seed = proximosTerrenosValidos (Mapa x l) !! mod seed (length (proximosTerrenosValidos (Mapa x l)))   
 
 
-
-
 --função auxiliar que dá os obstaculos válidos para uma linha com um terreno prédefinida 
 --obs:alterei a função para que não desse apenas os obstaculos válidos mas para que desse logo uma lista de obstaculos aleatórios 
 
 proximosObstaculosValidos :: Int ->Int-> (Terreno, [Obstaculo]) -> [Obstaculo]
 proximosObstaculosValidos lar seed (te,obs) |lar==length obs = obs 
-                                            |length obs == (lar-1) && elem Nenhum obs == False =obs ++ [Nenhum]
-                                            |otherwise =proximosObstaculosValidos lar seed (te,obs ++ [((proximosObstaculosValidosaux lar (te,obs)) !! mod seed (length (proximosObstaculosValidosaux lar (te,obs)) ))])
+                                            |length obs == (lar-1) && elem Nenhum obs == False = obs ++ [Nenhum] 
+                                            |otherwise =proximosObstaculosValidos lar seed (te,[((proximosObstaculosValidosaux lar (te,obs)) !! mod seed (length (proximosObstaculosValidosaux lar (te,obs)) ))]++obs)
               where 
                     proximosObstaculosValidosaux x (Rio v,[])=[Nenhum,Tronco]
                     proximosObstaculosValidosaux x (Estrada v,[]) = [Nenhum,Tronco]
@@ -50,6 +45,7 @@ proximosObstaculosValidos lar seed (te,obs) |lar==length obs = obs
                     proximosObstaculosValidosaux x (Estrada v,(Carro:Carro:Carro:t))=[Nenhum]
                     proximosObstaculosValidosaux x (Estrada v,obs)=[Nenhum,Carro]
                     proximosObstaculosValidosaux x (Relva,obs) =[Nenhum,Arvore] 
+
 
 
 
