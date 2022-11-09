c79f7ba2e9287866c9b43d520fdacf1cdf466251
a104010 <a104010@alunos.uminho.pt>
2022-11-09 12:29:55 +0000

    Tarefa 3 iniciada

--- src/Tarefa3_2022li1g029.hs
+++ src/Tarefa3_2022li1g029.hs
@@ -11,4 +11,50 @@ module Tarefa3_2022li1g029 where
 import LI12223
 
 animaJogo :: Jogo -> Jogada -> Jogo
-animaJogo = undefined
+animaJogo (Jogo (Jogador (x,y)) (Mapa l ((t,ob1:obs):tf))) m = Jogo (Jogador (pos (x,y) m t)) (Mapa l (mapamove ((t,ob1:obs):tf)))
+                                               where
+                                                  pos (x,y) Parado (Rio v ) |checktronco x (checkline (Jogo (Jogador (x,y)) (Mapa l ((t,ob1:obs):tf))))==True = (x+v,y)
+                                                                            |otherwise = (x,y)
+                                                                   
+                                                  pos (x,y) (Move Cima) t = (x,y+1)
+                                                  pos (x,y) (Move Baixo) t = (x,y-1)
+                                                  pos (x,y) (Move Esquerda) t =(x-1,y)
+                                                  pos (x,y) (Move Direita) t =(x+1,y)
+
+checkline::Jogo->[Obstaculo]
+checkline (Jogo (Jogador (x,y)) (Mapa l ((t,obs):tf))) |y==0 =obs
+                                                       |otherwise = checkline (Jogo (Jogador (x,y-1))(Mapa l (tf)))
+                                                     
+checktronco::Int->[Obstaculo]->Bool
+checktronco x (ob1:obs) |x==0 && ob1 == Tronco = True 
+                        |x==0 && ob1 /= Tronco = False 
+                        |otherwise = checktronco (x-1) obs 
+                                                     
+
+
+
+mapamove::[(Terreno,[Obstaculo])]->[(Terreno,[Obstaculo])]
+mapamove [(t,obs)] = [(t,obsmove (t,obs))]
+mapamove ((t,obs):fs) = (t,obsmove (t,obs)):mapamove fs
+               
+
+
+obsmove::(Terreno,[Obstaculo])->[Obstaculo]
+obsmove (Relva,obs) = obs
+obsmove (Rio v,obs)  |v==0 = obs
+                     |v>0 = obsmove (Rio (v-1),(last obs : init obs ))
+                     |otherwise= obsmove (Rio (v+1),tail obs ++ [head obs] )
+
+obsmove (Estrada v,obs) |v==0 = obs
+                        |v>0 = obsmove (Estrada (v-1),last obs: init obs )
+                        |otherwise = obsmove (Estrada (v+1),tail obs ++ [head obs])
+
+
+
+
+
+
+
+                
+
+
