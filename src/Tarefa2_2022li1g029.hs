8f807abaeed027aa4c87c13be1325a04ea1003df
a104010 <a104010@alunos.uminho.pt>
2022-11-10 00:02:18 +0000

    Tarefa2 ainda cria sequencias de obstaculos invalidos

--- src/Tarefa2_2022li1g029.hs
+++ src/Tarefa2_2022li1g029.hs
@@ -11,60 +11,71 @@ module Tarefa2_2022li1g029 where
 import LI12223
 import Data.List.NonEmpty (groupAllWith1)
 
-
-
-
---função auxiliar que dá os terrenos válidos para a proxima linha
-
-proximosTerrenosValidos :: Mapa -> [Terreno]
-proximosTerrenosValidos (Mapa l []) = [Rio 0, Estrada 0, Relva]                                                                       --quando não tem nenhum terreno
-proximosTerrenosValidos (Mapa l (t:[(Rio v1, ob1),(Rio v2, ob2),(Rio v3, ob3),(Rio v4, ob4)])) = [Estrada 0, Relva]                   --exceção: quando tem 4 rios seguidos 
-proximosTerrenosValidos (Mapa l (t:[(Estrada v1,ob1),(Estrada _ ,_),(Estrada _,_),(Estrada _,_),(Estrada _, _)]) )=[Rio 0, Relva]     --exceção: quando tem 5 estradas seguidas
-proximosTerrenosValidos (Mapa l (t:[(Relva ,_),(Relva ,_),(Relva ,_),(Relva ,_),(Relva ,_)]))=[Estrada 0 , Rio 0]                     --exceção: quando tem 5 relvas seguidas                                           
-proximosTerrenosValidos (Mapa l t) = [Rio 0, Estrada 0, Relva]                                                                        --restantes casos
-
-
---função auxiliar que dá o terreno aleatório da proxima linha 
-
-terrenoaleatorio::Mapa->Int->Terreno
-terrenoaleatorio (Mapa x l) seed = proximosTerrenosValidos (Mapa x l) !! mod seed (length (proximosTerrenosValidos (Mapa x l)))   
-
-
---função auxiliar que dá os obstaculos válidos para uma linha com um terreno prédefinida 
-
-
 group'::Eq a => [a] -> [[a]]
 group'[]=[]
 group'[x]=[[x]]
 group' (h:t) |elem h (head r) = (h: (head r)):tail r 
              |otherwise = [h]: r 
-                  where r=group' t
-
-
-obslistrandom :: Int ->Int-> (Terreno, [Obstaculo]) -> [Obstaculo]
-obslistrandom lar seed (Rio v ,obs)|lar==length obs =obs
-                                   |length obs == (lar-1) && length (head (group' (obslistrandom lar seed (Rio v ,obs)))) + length (last (group' (obslistrandom lar seed (Rio v ,obs)))) >=5= [Nenhum] 
-                                   |length obs == (lar-1) && elem Tronco obs ==False = obs ++[Tronco]
-                                   |otherwise =obslistrandom lar (seed-1) (Rio v,[((proximosObstaculosValidos lar (Rio v,obs)) !! mod seed (length (proximosObstaculosValidos lar (Rio v,obs)) ))]++obs)
-obslistrandom lar seed (te,obs) |lar==length obs = obs 
-                                            |length obs == (lar-1) && elem Nenhum obs == False = obs ++ [Nenhum] 
-                                            |otherwise =obslistrandom lar (seed-1) (te,[((proximosObstaculosValidos lar (te,obs)) !! mod seed (length (proximosObstaculosValidos lar (te,obs)) ))]++obs)
-
-proximosObstaculosValidos x (Rio v,[])=[Nenhum,Tronco]
-proximosObstaculosValidos x (Estrada v,[]) = [Nenhum,Tronco]
-proximosObstaculosValidos x (Relva,[])=[Nenhum,Arvore]
-proximosObstaculosValidos x (Rio v ,(Tronco:Tronco:Tronco:Tronco:Tronco:t))=[Nenhum]
-proximosObstaculosValidos x (Rio v,obs)=[Nenhum,Tronco]
-proximosObstaculosValidos x (Estrada v,(Carro:Carro:Carro:t))=[Nenhum]
-proximosObstaculosValidos x (Estrada v,obs)=[Nenhum,Carro]
-proximosObstaculosValidos x (Relva,obs) =[Nenhum,Arvore] 
+                  where r = group' t
 
 
 
+proximosTerrenosValidos :: Mapa -> [Terreno]
+proximosTerrenosValidos (Mapa lar [(Estrada v1 ,obs1),(Estrada v2 ,obs2),(Estrada v3 ,obs3),(Estrada v4 ,obs4),(Estrada v5 ,obs5)])=[Rio 0,Relva]
+proximosTerrenosValidos (Mapa lar (h:[(Estrada  v1,obs1),(Estrada  v2, obs2),(Estrada v3, obs3),(Estrada v4, obs4),(Estrada v5, obs5)]))=[Rio 0, Relva]
+proximosTerrenosValidos (Mapa lar [])=[Relva,Rio 0 ,Estrada 0]
+proximosTerrenosValidos (Mapa lar [(Rio v1, obs1),(Rio v2, obs2),(Rio v3, obs3),(Rio v4, obs4)]) = [Estrada 0, Relva]
+proximosTerrenosValidos (Mapa lar (h:[(Rio v1, obs1),(Rio v2, obs2),(Rio v3, obs3),(Rio v4, obs4)])) = [Estrada 0, Relva]
+proximosTerrenosValidos (Mapa lar [(Relva ,obs1),(Relva, obs2),(Relva , obs3),(Relva , obs4),(Relva , obs5)])=[Estrada 0 , Rio 0]
+proximosTerrenosValidos (Mapa lar (h:[(Relva ,obs1),(Relva, obs2),(Relva , obs3),(Relva , obs4),(Relva , obs5)]))=[Estrada 0 , Rio 0]
+proximosTerrenosValidos (Mapa lar t)=[Relva,Rio 0 ,Estrada 0]
 
---função principal que através das funções auxiliares estende o mapa
 
-estendeMapa::Mapa ->Int->Mapa
-estendeMapa (Mapa lar filas) seed = Mapa lar (filas ++ [(ta ,obslistrandom lar seed ((ta,[])))])
-                                                                     where ta=terrenoaleatorio (Mapa lar filas)  seed 
+proximosObstaculosValidos :: Int-> (Terreno, [Obstaculo]) -> [Obstaculo]
+proximosObstaculosValidos lar (x,y) |length y== lar = [] 
+                                    |length y == (lar-1) && elem Nenhum y == False = [Nenhum]
+                                    |otherwise = proximosObstaculosValidosAux lar (x,y)
 
+{-}
+proximosObstaculosValidos lar (Rio v ,obs)|lar==length obs =[]
+                                          |head obs == Tronco && last obs == Tronco && length obs == (lar-1) && length (head (group' (proximosObstaculosValidos lar (Rio v ,obs)))) + length (last (group' (proximosObstaculosValidos lar  (Rio v ,obs)))) >=5= [Nenhum] 
+                                          |length obs == (lar-1) && elem Tronco obs ==False = [Tronco]
+                                          |otherwise = proximosObstaculosValidosAux lar (Rio v,obs)
+proximosObstaculosValidos lar  (Relva,obs) |lar==length obs = []
+                                           |length obs == (lar-1) && elem Nenhum obs == False = obs ++ [Nenhum] 
+                                           |otherwise = proximosObstaculosValidosAux lar  (Relva,obs) 
+proximosObstaculosValidos lar  (Estrada v,obs) |lar==length obs = []
+                                       |head obs == Carro && last obs == Carro && length obs == (lar-1) && length (head (group' (proximosObstaculosValidos lar  (Estrada v ,obs)))) + length (last (group' (proximosObstaculosValidos lar  (Estrada v ,obs)))) >=5= [Nenhum] 
+                                       |length obs == (lar-1) && elem Nenhum obs == False = obs ++ [Nenhum] 
+                                       |otherwise =proximosObstaculosValidosAux lar  (Estrada v,obs)
+
+-}
+
+
+
+proximosObstaculosValidosAux::Int-> (Terreno,[Obstaculo])->[Obstaculo]
+proximosObstaculosValidosAux a (Rio b, [])=[Nenhum, Tronco]
+proximosObstaculosValidosAux a (Estrada b,[])=[Nenhum, Carro]
+proximosObstaculosValidosAux a (Relva, [])=[Nenhum, Arvore]
+proximosObstaculosValidosAux a (Rio b,t)|last t ==Tronco && last (init t) ==Tronco && last (init (init t)) ==Tronco && last (init (init (init t))) ==Tronco&&last (init (init (init (init t)))) ==Tronco =[Nenhum]
+proximosObstaculosValidosAux a (Rio b,l)=[Nenhum,Tronco]
+proximosObstaculosValidosAux a (Estrada b,t:[Carro,Carro,Carro])=[Nenhum]
+proximosObstaculosValidosAux a (Estrada b,l)=[Nenhum,Carro]
+proximosObstaculosValidosAux a (Relva , l)=[Nenhum,Arvore]
+
+
+
+
+
+randommap::Mapa->Int->Terreno
+randommap (Mapa lar l) seed = proximosTerrenosValidos (Mapa lar l) !! mod seed (length (proximosTerrenosValidos (Mapa lar l)))
+
+
+randomobs::Int->Int->[Obstaculo]->[Obstaculo]
+randomobs lar seed [] = []
+randomobs lar seed filas |lar==length filas = filas
+                     |(lar-1)==length filas && elem Nenhum filas == False =filas++[Nenhum]
+                     |otherwise = randomobs lar (seed+3) (filas++[filas !! mod (seed+1) (length filas)])
+
+estendeMapa :: Mapa -> Int -> Mapa
+estendeMapa (Mapa lar l) seed = Mapa lar (l ++ [(randommap (Mapa lar l) seed ,randomobs lar seed (proximosObstaculosValidos lar (randommap (Mapa lar l) seed , [])))])
