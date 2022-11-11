18f7f78f8278dbca5a591a3ba4319aa217ff00ec
Gonçalo Freitas <a104350@alunos.uminho.pt>
2022-11-11 00:35:05 +0000

    Falta Rio e estrada testar

--- src/Tarefa4_2022li1g029.hs
+++ src/Tarefa4_2022li1g029.hs
@@ -11,4 +11,27 @@ module Tarefa4_2022li1g029 where
 import LI12223
 
 jogoTerminou :: Jogo -> Bool
-jogoTerminou = undefined
+jogoTerminou (Jogo (Jogador (x,y)) (Mapa lr l)) = jogoTerminou1 (Jogo (Jogador (x,y)) (Mapa lr l))
+--MORREU PELA DIREITA
+jogoTerminou1 :: Jogo -> Bool 
+jogoTerminou1 (Jogo (Jogador (x,y)) (Mapa lr l)) = y > lr || y < 0 || x > length l
+                                
+jogoTerminou2 :: Jogo -> Bool
+jogoTerminou2 (Jogo (Jogador (x,y)) (Mapa lr [])) = True 
+jogoTerminou2 (Jogo (Jogador (x,y)) (Mapa lr (((Rio vr), []):t))) = jogoTerminou2 (Jogo (Jogador (x,y+1)) (Mapa lr t))
+jogoTerminou2  (Jogo (Jogador (x,y)) (Mapa lr (((Rio vr), (o1:ot)):t))) 
+                                |(o1 == Nenhum) && x == 0 && y == 0 = jogoTerminou2 (Jogo (Jogador (x+1,y)) (Mapa lr (((Rio vr), ot):t)))
+                                |(o1 == Tronco) && x == 0 && y == 0 = False 
+                                |otherwise = True 
+
+
+
+
+
+
+--carro se as coordenadas do carro estiverem no mesmo lugar que o boneco
+--rio se estiver no mesmo siteo que o rio -- para isso tenho de ver na lista de obstaculos 
+
+--type Coordenadas = (Int, Int)
+--data Jogador = Jogador Coordenadas
+--data Jogo = Jogo Jogador Mapa
