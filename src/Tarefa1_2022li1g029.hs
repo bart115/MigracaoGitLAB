ac65012929c81fc3b9eb30452e4581a03b975eb7
Gonçalo Freitas <a104350@alunos.uminho.pt>
2022-11-13 13:16:10 +0000

    Documentação está a meio

--- src/Tarefa1_2022li1g029.hs
+++ src/Tarefa1_2022li1g029.hs
@@ -16,11 +16,36 @@ definida por outras funções auxiliares como 'mapaValido1' , 'mapaValido2', 'ma
 cada uma desta têm funções especificas que serão explicadas mais tarde.
 
 ==Exemplo de Utilização:
- >>> mapaValido 
- >>>-}
+ >>> mapaValido (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco]),(Rio (-1),[Nenhum,Nenhum,Tronco]),(Relva,[Nenhum,Nenhum,Arvore]),(Estrada 1,[Carro,Nenhum,Carro]),(Rio 1,[Tronco,Nenhum,Tronco])]) 
+ >>> True 
+
+ >>> mapaValido (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco]),(Rio 1,[Nenhum,Nenhum,Tronco]),(Relva,[Nenhum,Nenhum,Arvore]),(Estrada 1,[Carro,Carro,Carro]),(Rio 1,[Tronco,Nenhum,Tronco])])]
+ >>> False-}
 mapaValido :: Mapa -> Bool
 mapaValido (Mapa lr l) = (mapaValido1 (Mapa lr l) && mapaValido2 (Mapa lr l)) && (mapaValido3 (Mapa lr l) && mapaValido4 (Mapa lr l)) && (mapaValido5 (Mapa lr l) && mapaValido6 (Mapa lr l)) && mapaValido7 (Mapa lr l)
 
+{-|Como referi anteriormentente a função principal, 'mapaValido' é composta por algumas auxiliares sendo uma delas a função 'mapaValido1',
+esta função recebe um mapa e confirma se nesse mapa consoante o terreno os obstaculos estão corretos.
+
+==Exemplo de Utilização:
+*Uma arvore num rio:
+@
+ >>> mapaValido1 (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Arvore])])
+ >>> False 
+@
+
+*Um Tronco no rio:
+@
+ >>> mapaValido1 (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco])])
+ >>> True
+@
+
+*Carros na Relva:
+@
+ >>> mapaValido1 (Mapa 3 [(Relva,[Nenhum,Nenhum,Carro]),(Estrada 3,[Nenhum,Nenhum,Carro]),(Estrada 3,[Nenhum,Nenhum,Arvore])])
+ >>> False
+@
+-}
 mapaValido1 :: Mapa -> Bool
 mapaValido1 (Mapa lr []) = True
 mapaValido1 (Mapa lr ((Relva,lo):t)) 
@@ -35,22 +60,45 @@ mapaValido1 (Mapa lr (((Estrada ve),lo):t))
         |(elem Arvore lo || elem Tronco lo) = False
         |otherwise = mapaValido1 (Mapa lr t)
 
+{-|A função 'mapaValido2' recebe um @Mapa@ e compara os terrenos, se o terrenos forem @Estrada@ , @Relva@ , @Rio@ e depois outra qualquer 
+a função faz a recursiva se os terrenos forem @Rio@ e depois outro @Rio@ a função compara as velocidades. Se ambas forem tiverem o mesmo sinal então ambos os rios estão a dirigir se 
+para a mesma direção. Esta função pretende que os seguidos nunca estejam a ir pela mesma direção.
+
+==Exemplos de Utilização:
+ >>> mapaValido2 (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco]),(Rio (-1),[Nenhum,Nenhum,Tronco]),(Relva,[Nenhum,Nenhum,Arvore]),(Rio (-1),[Nenhum,Nenhum,Carro]),(Rio 1,[Nenhum,Nenhum,Carro])])
+ >>> True
+
+ >>> mapaValido2 (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco]),(Rio (-1),[Nenhum,Nenhum,Carro]),(Relva,[Nenhum,Nenhum,Arvore]),(Rio 1,[Nenhum,Nenhum,Carro]),(Rio 1,[Nenhum,Nenhum,Carro])])
+ >>> False-}
 --2--
 mapaValido2 :: Mapa -> Bool
 mapaValido2 (Mapa lr []) = True
 mapaValido2 (Mapa lr (((Estrada ve),lo):t)) = mapaValido2 (Mapa lr t)
 mapaValido2 (Mapa lr ((Relva,lo):t)) = mapaValido2 (Mapa lr t)
 
---se o proximo valor não for rio
+
 mapaValido2 (Mapa lr (((Rio vr), lo) : ((Estrada ve),lo1) : t )) = mapaValido2 (Mapa lr t)
 mapaValido2 (Mapa lr (((Rio vr), lo) : (Relva,lo1) :t)) = mapaValido2 (Mapa lr t)
 
---
+
 mapaValido2 (Mapa lr [a]) = True  
 mapaValido2 (Mapa lr (((Rio vr),lo) : ((Rio vr1),lo1) : t))
                 |(vr > 0 && vr1 > 0) || (vr < 0 && vr1 < 0) = False
                 |otherwise = mapaValido2 (Mapa lr (((Rio vr1),lo1):t))
---3--
+
+{-|A função 'mapaValido3' tem como objetivo contar a quantidade de troncos que existe num rio e caso esse valor for maior que 
+5 então a função diz que @Mapa@ não é valido , para isso é utilizado uma função auxiliar chamada 'aux3' que junta os elementos iguais da lista 
+de obstaculos em listas, assim é só pedir o comprimento da lista se a cabeça da lista de obstaculos for Tronco.
+Nesta função existe uma exceção que caso na lista de obstaculos o primeiro elemento for tronco e igual ao ultimo então, ter se á de somar o comprimento do primeiro elemento da lista 
+com o comprimento do ultimo elemento da lista dos auxiliares.
+
+==Exeplos de Utilização:
+ >>> mapaValido3 (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco]),(Rio (-1),[Nenhum,Nenhum,Tronco]),(Relva,[Nenhum,Nenhum,Arvore]),(Rio 1,[Nenhum,Nenhum,Carro]),(Rio 1,[Tronco,Tronco,Tronco,Nenhum,Nenhum,Tronco,Tronco])])
+ >>> True
+
+ >>> mapaValido3 (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco]),(Rio (-1),[Nenhum,Nenhum,Tronco]),(Relva,[Nenhum,Nenhum,Arvore]),(Rio 1,[Nenhum,Nenhum,Carro]),(Rio 1,[Tronco,Tronco,Tronco,Nenhum,Tronco,Tronco,Tronco])])
+ >>> False-}
+
 mapaValido3 :: Mapa -> Bool
 mapaValido3 (Mapa lr (((Estrada ve),lo):t)) = mapaValido3 (Mapa lr t)
 mapaValido3 (Mapa lr ((Relva,lo):t)) = mapaValido3 (Mapa lr t)
@@ -62,17 +110,28 @@ mapaValido3 (Mapa lr (((Rio vr),(o1:ot)):t))
                                |(o1 == Tronco) && ((length (head (aux3 (o1:ot)))) > 5) = False
                                |otherwise = mapaValido3 (Mapa lr t)
 
+{-|Esta função 'aux3' simplesmente recebe a lista dos obstaculos e cria uma lista de listas com os elementos iguais dessa lista juntos
+Utilizando o exemplo da função 'mapaValido3'.
+
+==Exemplos de Utilização:
+ 
+ >>> aux3 [Tronco,Tronco,Tronco,Nenhum,Nenhum,Tronco,Tronco]
+ >>> [[Tronco,Tronco,Tronco],[Nenhum,Nenhum],[Tronco,Tronco]]
+
+ >>> aux3 [Tronco,Tronco,Tronco,Nenhum,Nenhum]
+ >>> [[Tronco,Tronco,Tronco],[Nenhum,Nenhum]]
+ -}
 aux3 :: Eq a => [a] -> [[a]]
 aux3 [] = []
 aux3 (h:t) = (h:takeWhile (== h) t) : aux3 (dropWhile (== h) t)
 
 
+{-|A função 'mapaValido4' funciona da mesma maneira que a função 'mapaValido3' mas com @Carro@ e com a auxiliar 'aux4' .
 
---mapaValido3 (Mapa 4 [(Rio 3 ,[Nenhum,Nenhum,Tronco])]) usar para testar, pois nao funciona
---usar também lista com mais de um elemento 
-
- 
---4--
+==Exemplo de Utilização: 
+ >>> mapaValido4 (Mapa 3 [(Estrada 3,[Nenhum,Nenhum,Carro]),(Rio (-1),[Nenhum,Nenhum,Tronco]),(Relva,[Nenhum,Nenhum,Arvore]),(Rio 1,[Nenhum,Nenhum,Carro]),(Rio 1,[Tronco,Tronco,Tronco,Nenhum,Nenhum,Tronco,Tronco])])
+ >>> True
+-}
 mapaValido4 :: Mapa -> Bool
 mapaValido4 (Mapa lr (((Rio vr),lo):t)) = mapaValido4 (Mapa lr t)
 mapaValido4 (Mapa lr ((Relva,lo):t)) = mapaValido4 (Mapa lr t)
@@ -84,11 +143,20 @@ mapaValido4 (Mapa lr (((Estrada ve),(o1:ot)):t))
                                |(o1 == Carro) && ((length (head (aux4 (o1:ot)))) > 3) = False
                                |otherwise = mapaValido3 (Mapa lr t)
 
+{-|A função auxiliar 'aux4' funciona da mesma maneira que a função 'aux3'.-}
 aux4 :: Eq a => [a] -> [[a]]
 aux4 [] = []
 aux4 (h:t) = (h:takeWhile (== h) t) : aux4 (dropWhile (== h) t)
 
---5--
+{-|A função 'mapaValido5' faz com que ,em todas as listas de obstaculos, exista pelo menos um elemento @Nenhum@, ou seja, uma linha não pode ser formada somente por obstáculos
+
+==Exemplo de Utilização: 
+ >>> mapaValido5 (Mapa 3 [(Rio 3,[Nenhum,Nenhum,Tronco]),(Rio (-1),[Nenhum,Nenhum,Tronco])
+ >>> True 
+
+ >>> mapaValido5 (Mapa 3 [(Rio 3,[Tronco,Tronco,Tronco]),(Rio (-1),[Nenhum,Nenhum,Tronco])
+ >>> False
+ -}
 mapaValido5 :: Mapa -> Bool
 mapaValido5 (Mapa lr []) = True 
 mapaValido5 (Mapa lr ((Relva,lo):t))
